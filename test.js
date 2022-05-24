ee07ee122ea70e76637fb7ea5354ef2d530e0ef9
Marco Guidara <marco.guidara@efesto.studio>
2022-05-24 16:50:20 +0200

    Add tests

--- test.js
+++ test.js
@@ -34,28 +34,38 @@ function generatePluginCss(overrides) {
 test('utility classes can be generated', () => {
   return generatePluginCss().then(css => {
     expect(css).toMatchCss(`    
-    .example-utility-class {
-      display: block
+    .center-absolute-y {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
     }
-    `);
-  });
-});
-
-test('options can be customized', () => {
-  return generatePluginCss({
-    theme: {
-      quickCenter: {
-        YOUR_PLUGIN_CUSTOM_OPTION: true,
-      },
-    },
-  }).then(css => {
-    expect(css).toMatchCss(`    
-    .example-utility-class {
-      display: block
+    .center-absolute-x {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
     }
-    .custom-utility-class {
-      background-color: red
+    .center-absolute {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
     }
+    .center-flex-y {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+    .center-flex-x {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    .center-flex {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }    
     `);
   });
 });
@@ -72,20 +82,136 @@ test('variants can be customized', () => {
     },
   }).then(css => {
     expect(css).toMatchCss(`
-    .example-utility-class {
-      display: block
-    } 
-    .hover\\:example-utility-class:hover {
-      display: block
-    } 
-    @media (min-width: 640px) {
-      .sm\\:example-utility-class {
-        display: block
-      }
-      .sm\\:hover\\:example-utility-class:hover {
-        display: block
-      }
+    .center-absolute-y {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
     }
+    .center-absolute-x {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+    .center-absolute {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+    .center-flex-y {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+    .center-flex-x {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    .center-flex {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+    .hover\\:center-absolute-y:hover {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+    .hover\\:center-absolute-x:hover {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+    .hover\\:center-absolute:hover {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+    .hover\\:center-flex-y:hover {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+    .hover\\:center-flex-x:hover {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    .hover\\:center-flex:hover {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+    @media (min-width: 640px) {
+      .sm\\:center-absolute-y {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+      .sm\\:center-absolute-x {
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+      }
+      .sm\\:center-absolute {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+      }
+      .sm\\:center-flex-y {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+      }
+      .sm\\:center-flex-x {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+      .sm\\:center-flex {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      }
+      .sm\\:hover\\:center-absolute-y:hover {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+      .sm\\:hover\\:center-absolute-x:hover {
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+      }
+      .sm\\:hover\\:center-absolute:hover {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+      }
+      .sm\\:hover\\:center-flex-y:hover {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+      }
+      .sm\\:hover\\:center-flex-x:hover {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+      .sm\\:hover\\:center-flex:hover {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      }
+    }    
     `);
   });
 });
